f8bbe744cff458af1618987a7eec54ba8f890a68
lnaginionis <lnaginionis@gmail.com>
2012-01-17 22:46:14 +0000

    + SvDesignPatterns.pas - added Factory Method pattern with demo project + TSvEnum<T> type for storing enumeration values

--- Types/uSvTypes.pas
+++ Types/uSvTypes.pas
@@ -26,7 +26,7 @@ type
   /// </example>
 {$ENDREGION}
   TPathBuilder = record
-  strict private
+  private
     sPath: string;
   public
     function Add(const APath: string; IncludeTrailingDelimiter: Boolean = True): TPathBuilder;
@@ -35,9 +35,37 @@ type
     class function Init(): TPathBuilder; static;
     class function InitCurrentDir(): TPathBuilder; static;
     class function InitHomePath(): TPathBuilder; static;
+    class function InitCustomPath(const APath: string): TPathBuilder; static;
+  end;
+
+  /// <summary>
+  /// Generic enumeration type
+  /// <remarks>
+  /// Should be used only for enumerations!
+  /// </remarks>
+  /// </summary>
+  TSvEnum<T> = record
+  private
+    FValue: NativeInt;
+  private
+    function GetValue: T;
+    procedure SetValue(const Value: T);
+  public
+    class operator Implicit(const AEnum: TSvEnum<T>): T; inline;
+    class operator Implicit(const AEnum: T): TSvEnum<T>; inline;
+    class operator Implicit(const AEnum: TSvEnum<T>): Integer; inline;
+    class operator Implicit(const AEnum: Integer): TSvEnum<T>; inline;
+
+    function ToString(): string;
+
+    property Value: T read GetValue write SetValue;
   end;
 
 implementation
+
+uses
+  Rtti,
+  TypInfo;
 
 { TPathBuilder }
 function TPathBuilder.Add(const APath: string;
@@ -71,6 +99,11 @@ begin
   Result.sPath := IncludeTrailingPathDelimiter(ExtractFileDir(ParamStr(0)));
 end;
 
+class function TPathBuilder.InitCustomPath(const APath: string): TPathBuilder;
+begin
+  Result.sPath := IncludeTrailingPathDelimiter(APath);
+end;
+
 class function TPathBuilder.InitHomePath: TPathBuilder;
 begin
   Result.sPath := IncludeTrailingPathDelimiter(GetHomePath);
@@ -80,5 +113,48 @@ function TPathBuilder.ToString: string;
 begin
   Result := sPath;
 end;
+
+{ TSvEnum<T> }
+
+function TSvEnum<T>.GetValue: T;
+begin
+  Result := TValue.FromOrdinal(TypeInfo(T), FValue).AsType<T>;
+end;
+
+class operator TSvEnum<T>.Implicit(const AEnum: TSvEnum<T>): T;
+begin
+  Result := AEnum.Value;
+end;
+
+class operator TSvEnum<T>.Implicit(const AEnum: T): TSvEnum<T>;
+begin
+  Result.Value := AEnum;
+end;
+
+class operator TSvEnum<T>.Implicit(const AEnum: TSvEnum<T>): Integer;
+begin
+  Result := AEnum.FValue;
+end;
+
+class operator TSvEnum<T>.Implicit(const AEnum: Integer): TSvEnum<T>;
+begin
+  Result.FValue := AEnum;
+end;
+
+procedure TSvEnum<T>.SetValue(const Value: T);
+var
+  AValue: TValue;
+begin
+  AValue := TValue.From<T>(Value);
+  if AValue.Kind = tkEnumeration then
+    FValue := AValue.AsOrdinal;
+end;
+
+function TSvEnum<T>.ToString: string;
+begin
+  Result := GetEnumName(TypeInfo(T), FValue);
+end;
+
+
 
 end.
